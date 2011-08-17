0ac0e536e1c02263014656b1394cf5a608a3c6d7
romaine carter <xpwiz@hotmail.com>
2011-08-17 10:27:30 -0700

    Uploaded

---
+++ simpleGA/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace simpleGA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblWinner = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblSample = new System.Windows.Forms.Label();
+            this.lblIterations = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 110);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 11);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Winner Count:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 42);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sample:";
+            // 
+            // lblWinner
+            // 
+            this.lblWinner.AutoSize = true;
+            this.lblWinner.Location = new System.Drawing.Point(124, 11);
+            this.lblWinner.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblWinner.Name = "lblWinner";
+            this.lblWinner.Size = new System.Drawing.Size(20, 17);
+            this.lblWinner.TabIndex = 1;
+            this.lblWinner.Text = "...";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 42);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 17);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Sample:";
+            // 
+            // lblSample
+            // 
+            this.lblSample.AutoSize = true;
+            this.lblSample.Location = new System.Drawing.Point(124, 41);
+            this.lblSample.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSample.Name = "lblSample";
+            this.lblSample.Size = new System.Drawing.Size(20, 17);
+            this.lblSample.TabIndex = 1;
+            this.lblSample.Text = "...";
+            // 
+            // lblIterations
+            // 
+            this.lblIterations.AutoSize = true;
+            this.lblIterations.Location = new System.Drawing.Point(124, 73);
+            this.lblIterations.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblIterations.Name = "lblIterations";
+            this.lblIterations.Size = new System.Drawing.Size(20, 17);
+            this.lblIterations.TabIndex = 1;
+            this.lblIterations.Text = "...";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 74);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 17);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Iterations:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 153);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblIterations);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblSample);
+            this.Controls.Add(this.lblWinner);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblWinner;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblSample;
+        private System.Windows.Forms.Label lblIterations;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ simpleGA/Form1.cs
@@ -0,0 +1,185 @@
+﻿/*
+Author:   Romaine A. Carter
+Created:  Thursday, January 06. 2011
+Rivision: Null
+Descrip:  Genetic algorithm inspired timetable generation engine version 1.0
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Linq.Expressions;
+using System.Threading;
+
+namespace simpleGA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();           
+        }
+
+        private Thread _thread;
+        private const int epochs             = 1000000;         // epoch = number of iterations. 
+        private const int populationSize     = 1000;            // size of the poulation in an epoch.
+        private const int initPopulationSize = 10000;           // initial population size
+        private const int survivorThreshold  = 6;               // chromosomes with fitness >= survivorThreshold will survive.
+        private const int mutantThreshold    = 3;               // chromosomes with fitness >= mutantThreshold will be mutated.
+        private const int crossoverThreshold = 6;               // chromosomes with fitness >= crossoverThreshold will be mated.
+        private const int fitnessUBound      = 9;               // end algo if fitness between fitnessUBound and fitnessLBound.
+        private const int fitnessLBound      = 9;               // end algo if fitness between fitnessUBound and fitnessLBound.
+
+        readonly List<sequence> survivors  = new List<sequence>();          
+
+           
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _thread = new Thread(new ThreadStart(Start));
+            _thread.Start();
+        }
+
+        private void Start()
+        {
+            var population = new List<sequence>();
+        
+            InitGA(population, initPopulationSize);
+
+            var count = 0;
+            while (population.Count(i => i.fitness >= fitnessLBound && i.fitness <= fitnessUBound ) <= 0 && count <= epochs)
+            {                
+                Epoch(population);
+                count++;
+                SetIteration(count.ToString());
+
+                var found = population.Where(i => i.fitness >= 8).FirstOrDefault();
+                if(found != null)
+                    SetSample(string.Join("", found.buffer));
+            }            
+        }
+
+        private void Epoch(List<sequence> population)
+        {
+            //Elitism
+            survivors.AddRange(population.Where(i => i.fitness >= survivorThreshold)); 
+            
+            Mutate(population.Where(i => i.fitness >= mutantThreshold));
+               
+            CrossOver(population.Where(i => i.fitness >= crossoverThreshold) as IEnumerable<sequence>);           
+          
+            population.Clear();
+            population.AddRange(survivors);
+
+            for (int i = 0; i < populationSize - survivors.Count; i++) 
+            {
+                var temp = GenerateSequence();
+                CalculateFitness(temp);
+                population.Add(temp);
+            }
+            survivors.Clear();
+        }
+
+        private static void InitGA(List<sequence> generation, int size)
+        {
+            for (int i = 0; i < size; i++) // generate population
+            {
+                var temp = GenerateSequence();
+                CalculateFitness(temp);
+                generation.Add(temp);                
+            }
+        }
+
+        private static void CalculateFitness(sequence seq)
+        {
+            var ordered = new sequence{buffer = new List<string>() {"1", "2", "3", "4", "5", "6", "7", "8", "9"}};
+                                                                                              
+            int result=0;
+            for (int i = 0; i < 9; i++)
+            {
+                if (ordered.buffer[i] == seq.buffer[i]) result++;                
+            }
+            seq.fitness = result;
+        }
+
+        private readonly Random rn = new Random();
+        private void Mutate(IEnumerable<sequence> candidates)
+        {
+            try
+            {                
+                foreach (var individual in candidates)   // mutate not so fit chromosomes in population.
+                {
+                    int index = rn.Next(0, 9);
+                    individual.buffer[index] = rn.Next(0, 10).ToString();
+                    CalculateFitness(individual);
+                    survivors.Add(individual);
+                }
+            }
+            catch (Exception)  {}                    
+        }
+
+        private void CrossOver(IEnumerable<sequence> candidates)
+        {
+           if(candidates.Count() >= 2)
+            {
+                var temp = new sequence();
+                temp.buffer = new List<string>();
+
+                temp.buffer.AddRange(candidates.ElementAt(0).buffer.GetRange(0,3));
+                temp.buffer.AddRange(candidates.ElementAt(1).buffer.GetRange(3, 6));
+                CalculateFitness(temp);
+                survivors.Add(temp);               
+            }                               
+        }
+
+        static readonly Random rand = new Random();
+        private static sequence GenerateSequence()
+        {   
+            var str = new List<string>();                               
+            for(int i = 1; i < 10; i++)
+            {                
+                str.Add(rand.Next(0, 10).ToString());              
+            }
+            var temp = new sequence();
+            temp.buffer = str;
+            return temp ;
+        }
+
+        delegate void SetTextCallback(string text);
+        private void SetIteration(string text)
+        {           
+            if (lblIterations.InvokeRequired)
+            {
+                var d = new SetTextCallback(SetIteration);
+                Invoke(d, new object[] { text });
+            }
+            else
+            {
+                lblIterations.Text = text;
+            }
+        }        
+
+        private void SetSample(string text)
+        {          
+            if (lblSample.InvokeRequired)
+            {
+                var d = new SetTextCallback(SetSample);
+                Invoke(d, new object[] { text });
+            }
+            else
+            {
+                lblSample.Text = text;
+            }
+        }
+    }
+
+    public class sequence
+    {
+        public int fitness;
+        public List<string> buffer;
+    }
+}
